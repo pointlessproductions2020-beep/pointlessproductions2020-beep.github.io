ca26c7f482062b8798601e092e31e864d350c7c1
pointlessproductions2020-beep <pointlessproductions2020@gmail.com>
2026-08-01 18:39:17 +0100

    Update viewer.js

--- tools/paintlessuv/js/uv/viewer.js
+++ tools/paintlessuv/js/uv/viewer.js
@@ -7,6 +7,10 @@ export const uvView = {
 
   zoom: 1,
 
+  minZoom: 0.25,
+
+  maxZoom: 20,
+
   offsetX: 0,
 
   offsetY: 0,
@@ -18,3 +22,46 @@ export const uvView = {
   lastY: 0
 
 };
+
+
+/* =========================================================
+   INITIALISE
+========================================================= */
+
+export function initialiseUVViewer(
+  canvas,
+  redraw
+) {
+
+  canvas.addEventListener(
+    "wheel",
+    (event) => {
+
+      event.preventDefault();
+
+      const direction =
+        event.deltaY < 0
+          ? 1.1
+          : 0.9;
+
+      uvView.zoom *=
+        direction;
+
+      uvView.zoom =
+        Math.min(
+          uvView.maxZoom,
+          Math.max(
+            uvView.minZoom,
+            uvView.zoom
+          )
+        );
+
+      redraw();
+
+    },
+    {
+      passive: false
+    }
+  );
+
+}
